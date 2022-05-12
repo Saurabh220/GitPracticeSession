9041e078278b764d7923669c03276eb3fef1e9d6
Saurabh220 <saurabha25252@gmail.com>
2022-05-13 01:07:36 +0530

    New project for developer

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//int n=7;
+		System.out.println("enter a number = ");
+		Scanner sc =new Scanner(System.in);
+		Integer no=sc.nextInt();
+		int count =0;
+		for(int i=1;i<=no;i++) {
+			if(no%i==0) {
+				count++;
+			}
+	}
+		if(count==2) {
+			System.out.println("prime number " +no);
+			
+		}else {
+			System.out.println("not a prme num " +no);
+		}
+
+}
+}
